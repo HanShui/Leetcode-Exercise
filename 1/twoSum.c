19b9dc504ca8bee4a39644319b98c1619dea205c
sjhong <sjhong@cupdata.com>
2019-02-15 15:15:51 +0800

    Leetcod

---
+++ 1/twoSum.c
@@ -0,0 +1,22 @@
+int* twoSum(int* nums, int numsSize, int target) {
+  int* results = (int*) malloc (sizeof(int) * 2);
+  results[0] = 0;
+  results[1] = 1;
+  if(numsSize <= 1) return results;
+  int* dists = (int*) malloc (sizeof(int) * numsSize);
+  for(int i = 0; i < numsSize; i++)
+    {
+      dists[i] = target - nums[i];
+      for(int j = i+1; j <numsSize; j++)
+        {
+          if(dists[i] == nums[j])
+            {
+              results[0] = i;
+              results[1] = j;
+              free(dists);
+              return results;
+            }
+        }
+    }
+  return results;
+}
